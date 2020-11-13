7e52542f22846648ad43d48336a40f1893077c45
sebastian leon <sebastian leon>
2020-11-12 20:28:39 -0500

    q

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(rew, res){
+    res.sendfile(_dirname + '/views/index.html');
+});
+
+app.listen(9000);
+
+console.log("Bienvenidos", app.settings.env);
