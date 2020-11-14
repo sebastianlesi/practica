d8d38da646af8688e28231de326d891894d290eb
sebastian leon <sebastian leon>
2020-11-13 23:40:15 -0500

    j

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(rew, res){
-    res.sendfile(_dirname + '/views/index.html
+    res.sendfile(_dirname + '/views/index.html');
+});
 
 app.listen(9000);
 
