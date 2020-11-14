535a89888afa017c79bebbfc54c3147f9ce38da6
sebastian leon <sebastian leon>
2020-11-14 08:30:06 -0500

    h

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(rew, res){
-    res.sendfile(_dirname + '/views/index.html
+    res.sendfile(_dirname + '/views/index.html');
+});
 
 app.listen(9000);
 
-console.log("Bienvenidos", app.settings.env);
+console.log("Prueba", app.settings.env);
